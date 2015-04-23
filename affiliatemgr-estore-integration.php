7b1454b4e741274647b53f2dfa2f1dbbe5b0d7ab
affmngr <affmngr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-23 02:51:40 +0000

    . first commit
    git-svn-id: https://plugins.svn.wordpress.org/affiliates-manager-wp-estore-integration/trunk@1142804 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ affiliatemgr-estore-integration.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Plugin Name: Affiliates Manager WP eStore Integration
+Plugin URI: https://wpaffiliatemanager.com
+Description: Process an affiliate commission via Affiliates Manager after a WP eStore checkout.
+Version: 1.0
+Author: wp.insider, wpaffiliatemgr, affmngr
+Author URI: https://wpaffiliatemanager.com
+*/
+
+function wpam_eStore_database_updated_after_payment($payment_data, $cart_items)
+{
+    $custom_data = $payment_data['custom'];
+    WPAM_Logger::log_debug('WP eStore Integration - database updated hook fired. Custom field value: '.$custom_data);
+    $custom_values = array();
+    parse_str($custom_data, $custom_values);
+    if(isset($custom_values['wpam_tracking']) && !empty($custom_values['wpam_tracking']))
+    {
+        $tracking_value = $custom_values['wpam_tracking'];
+        WPAM_Logger::log_debug('WP eStore Integration - Tracking data present. Need to track affiliate commission. Tracking value: '.$tracking_value);
+        $purchaseLogId = $payment_data['txn_id'];
+        $purchaseAmount = $payment_data['mc_gross']; //TODO - later calculate sub-total only
+        $strRefKey = $tracking_value;
+        $requestTracker = new WPAM_Tracking_RequestTracker();
+        $requestTracker->handleCheckoutWithRefKey( $purchaseLogId, $purchaseAmount, $strRefKey);
+        WPAM_Logger::log_debug('WP eStore Integration - Commission tracked for transaction ID: '.$purchaseLogId.'. Purchase amt: '.$purchaseAmount);
+    }
+}
+add_action("eStore_product_database_updated_after_payment", "wpam_eStore_database_updated_after_payment", 10, 2);
+
+function wpam_eStore_add_custom_parameters($custom_field_val)
+{
+    if(isset($_COOKIE[WPAM_PluginConfig::$RefKey]))
+    {
+        $name = 'wpam_tracking';
+        $value = $_COOKIE[WPAM_PluginConfig::$RefKey];
+        $new_val = $name.'='.$value;
+        $current_val = $custom_field_val;
+        if(empty($current_val)){
+            $custom_field_val = $new_val;
+        }
+        else{
+            $custom_field_val = $current_val.'&'.$new_val;
+        }
+        WPAM_Logger::log_debug('WP eStore Integration - Adding custom field value. New value: '.$custom_field_val);
+    }
+    return $custom_field_val;
+}
+
+add_filter("eStore_custom_field_value_filter", "wpam_eStore_add_custom_parameters");
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Affiliates Manager WP eStore Integration ===
+Contributors: wp.insider, wpaffiliatemgr, affmngr
+Donate link: https://wpaffiliatemanager.com/
+Tags: affiliate, affiliates, digital downloads, download, downloads, sell downloads, affiliates manager, integration, eStore, checkout
+Requires at least: 3.8
+Tested up to: 4.1
+Stable tag: 1.0
+License: GPLv2 or later
+
+Process an affiliate commission via Affiliates Manager after a WP eStore checkout
+
+== Description ==
+
+This addon allows you to integrate WP eStore with the Affiliates Manager plugin.
+
+When a user pay for an item on your site, this addon will check to see if the user was referred to your site by an affiliate. It will then give commission to the affiliate who referred the user.
+
+This addon requires the [Affiliates Manager](https://wordpress.org/plugins/affiliates-manager/) and [WP eStore](https://www.tipsandtricks-hq.com/wordpress-estore-plugin-complete-solution-to-sell-digital-products-from-your-wordpress-blog-securely-1059) plugins.
+
+Read the usage instructions [here](https://wpaffiliatemanager.com/affiliates-manager-sell-digital-downloads-integration/)
+
+== Installation ==
+
+1. Upload `affiliatemgr-estore-integration` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+None
+
+== Screenshots ==
+Check the following page for screenshots
+https://wpaffiliatemanager.com/screen-shots/
+
+== Changelog ==
+
+= 1.0 =
+* First commit to wordpress.org
+
+== Upgrade Notice ==
+None
